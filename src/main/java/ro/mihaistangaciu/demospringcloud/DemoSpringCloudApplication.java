82a9b4ae5f02080f5c23e2ad2d8ba6628b400e39
mstangaciu <mihai.stangaciu@gmail.com>
2019-09-29 11:56:43 +0300

    pushing Changes with Functional bean definition

--- src/main/java/ro/mihaistangaciu/demospringcloud/DemoSpringCloudApplication.java
+++ src/main/java/ro/mihaistangaciu/demospringcloud/DemoSpringCloudApplication.java
@@ -1,21 +1,29 @@
 package ro.mihaistangaciu.demospringcloud;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.function.Function;
 
-@SpringBootApplication
-public class DemoSpringCloudApplication {
+@SpringBootConfiguration
+public class DemoSpringCloudApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringCloudApplication.class, args);
+		FunctionalSpringApplication.run(DemoSpringCloudApplication.class, args);
 	}
 
-	@Bean
 	public Function<String, String> reverseString() {
 		return value -> new StringBuilder(value).reverse().toString();
 	}
 
+	@Override
+	public void initialize(GenericApplicationContext context) {
+		context.registerBean("demo", FunctionRegistration.class,
+				() -> new FunctionRegistration<>(reverseString())
+						.type(FunctionType.from(String.class).to(String.class)));
+	}
 }
